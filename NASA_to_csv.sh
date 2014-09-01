9a3e214d669f925af6b74cdc355dd9b5dd58b6a1
Paul De Schacht <pauldeschacht@gmail.com>
2014-09-01 19:47:50 +0200

    clean up filenames, add converted of NASA timestamp format

--- NASA_to_csv.sh
+++ NASA_to_csv.sh
@@ -1,15 +1,16 @@
+SEP=$'\t'	
 #
 # get the NASA logs
 #
-wget -X GET -O NASA.csv.gz ftp://ita.ee.lbl.gov/traces/NASA_access_log_Jul95.gz
+wget -X GET -O NASA_access_log_Jul95.gz ftp://ita.ee.lbl.gov/traces/NASA_access_log_Jul95.gz
 #
-# create tab-separated csv file. Note \t is tab character, for OSX systems, use CTRL+V TAB to insert tab in command
+# create tab-separated csv file. 
 #
-zcat NASA.csv | sed "s/ - - \[/\t/g" | sed "s/\] \"/\t/g" | sed "s/\" /\t/g" | sed "s/\([0-9]*\) \([0-9]*\)$/ \1\t\2 /g" > NASA.csv
+zcat NASA_access_log_Jul95.gz | sed "s/ - - \[/${SEP}/g" | sed "s/\] \"/${SEP}/g" | sed "s/\" /${SEP}/g" | sed "s/\([0-9]*\) \([0-9]*\)$/ \1${SEP}\2 /g" > NASA_raw.csv
 #
-# get the geoip information for each NASA weblog. Prerequisite: freegeoip, open source tool on 
+# get the geoip information for each NASA weblog. Prerequisite: freegeoip, open source tool on https://freegeoip.net/
 #
-./get_geoip.sh NASA.csv > geoip_raw.csv
+./NASA_to_geoip.sh NASA_raw.csv > geoip_raw.csv
 #
 # create file with country_code + country_name 
 #
@@ -18,3 +19,7 @@ grep -v "Not Found" geoip_raw.csv | cut -f 6-7 | sort | uniq > countries.csv
 # extract the country_name from the geoip file (only purpose is to add an extra join in the tutorial)
 #
 grep -v "Not Found" geoip_raw.csv | cut -f 1-6 > geoip.csv 
+#
+# convert timestamps to Impala timestamp format
+./NASA_fix_timestamp NASA_raw.csv > NASA.csv
+#
